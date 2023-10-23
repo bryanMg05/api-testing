3334ab7d40552b6f02132613ea2835856cf06097
bryanMg05 <zS21004530@estudiantes.uv.mx>
2023-10-23 15:16:14 -0600

    update api

--- index.js
+++ index.js
@@ -9,17 +9,50 @@ app.options('*', cors());
 const port = 8080;
 
 app.get('/', (req, res, next) => {
-	res.send('node express api <br> by bryan');
+	res.send('node express api <br> by Erick Espinosa');
 });
 
-app.get('/hours', (req, res, next) => {
-  res.sendfile("assets/1a_dev_hours.json");
+
+app.get('/header', (req, res, next) => {
+  res.sendfile("assets/header.json");
 });
 
-app.get('/size', (req, res, next) => {
+app.get('/work-experience', (req, res, next) => {
+	res.sendfile("assets/work-experience.json");
+});
+
+app.get('/skills', (req, res, next) =>{
+	res.sendfile("assets/skills.json");
+})
+
+app.get('/certifications', (req, res, next) =>{
+	res.sendfile("assets/certifications.json");
+})
+
+app.get('/size', (req, res, next) =>{
 	res.sendfile("assets/1a_proxy_size.json");
-  });
+})
+
+app.get('/hours', (req, res, next) =>{
+	res.sendfile("assets/1a_dev_hours.json");
+})
+
+app.get('/3a1', (req, res, next) => {
+	res.sendfile("assets/3a_test1.json");
+});
+
+app.get('/3a2', (req, res, next) => {
+	res.sendfile("assets/3a_test2.json");
+});
+
+app.get('/3a3', (req, res, next) => {
+	res.sendfile("assets/3a_test3.json");
+});
+
+app.get('/3a4', (req, res, next) => {
+	res.sendfile("assets/3a_test4.json");
+});
 
 app.listen(port,  () => 
-	console.log('listening on port ' + port
+	console.log('listening on http://localhost:8080/'
 ));
